b1bbe418047799aada180f50c434ec632e10ab88
robex99 <roberthy81@hotmail.com>
2020-09-28 14:51:55 -0300

    Realizei os exercícios com exceção do 4, não entendi mto bem a proposta.

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,76 @@
+// EXERCÍCIO 1
+
+// let nome
+
+// let idade
+
+// console.log(typeof idade)
+
+// // FOI IMPRESSO DESSE JEITO POIS A VARIAVEL AINDA N FOI DEFINIDA.
+
+// nome = prompt('Qual é o seu nome?')
+
+// idade = prompt('Qual é a sua idade?')
+
+// console.log(typeof idade)
+// console.log(typeof nome)
+
+// // AS DUAS FORAM CONSIDERADAS STRINGS.
+
+// console.log('Olá ' + nome + ', você tem ' + idade + ' anos' )
+
+// EXERCÍCIO 2
+
+// let comida
+// let pais
+// let carroFavorito
+// let corFavorita
+// let anos
+
+// comida = prompt('Qual sua comida favorita?')
+// pais = prompt('Qual seu país favorito?')
+// carroFavorito = prompt('Qual seu carro favorito?')
+// corFavorita =  prompt('Qual sua cor favorita?')
+// anos = prompt('Quantos anos você tem?')
+
+// console.log('1. Qual sua comida favorita?')
+// console.log('Resposta: ' + comida)
+// console.log('2. Qual seu pais favorito?')
+// console.log('Resposta: ' + pais)
+// console.log('3. Qual seu carro favorito?')
+// console.log('Resposta: ' + carroFavorito)
+// console.log('4. Qual sua cor favorita?')
+// console.log('Resposta: ' + corFavorita)
+// console.log('5. Quantos anos você tem?')
+// console.log('Resposta: ' + anos)
+
+
+// EXERCÍCIO 3
+
+// let comidaFav = ['arroz', 'feijao', 'café', 'pato', 'peixe']
+// console.log(comidaFav)
+
+// console.log('Essas são as minhas comidas preferidas:')^
+// console.log(comidaFav[0])
+// console.log(comidaFav[1])
+// console.log(comidaFav[2])
+// console.log(comidaFav[3])
+// console.log(comidaFav[4])
+
+// comidaFav[1] = prompt('Digite a sua comida preferida:')
+
+// console.log('Essas são as minhas comidas preferidas:')
+// console.log(comidaFav[0])
+// console.log(comidaFav[1])
+// console.log(comidaFav[2])
+// console.log(comidaFav[3])
+// console.log(comidaFav[4])
+
+// EXERCÍCIO 4
+
+let questions = ['Você está usando roupa rosa hoje?', 'Você está com calor?', 'Tudo bem com você?']
+let answers = [0,0,0]
+
+answers[0] = prompt(questions[0])
+answers[1] = prompt(questions[1])
+answers[2] = prompt(questions[2])
